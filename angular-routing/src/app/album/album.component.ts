b492e0d5507461e30c6fb862438abc58ec5e4a0f
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2020-04-21 09:51:15 +0000

    inizio componente album

--- angular-routing/src/app/album/album.component.ts
+++ angular-routing/src/app/album/album.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { SpotifyService } from '../spotify.service';
+import { Observable } from 'rxjs';
+import { Location } from '@angular/common';
 @Component({
-  selector: 'app-album',
-  templateUrl: './album.component.html',
-  styleUrls: ['./album.component.css']
+  selector: 'app-artist',
+  templateUrl: './artist.component.html',
+  styleUrls: ['./artist.component.css']
 })
-export class AlbumComponent implements OnInit {
+export class ArtistComponent implements OnInit {
+  //Osserva gli eventi sulla route tracks, restituisce la ParamMap che contiene tutti i
+  //parametri passati all’url
+  routeObs: Observable<ParamMap>;
 
-  constructor() { }
+  artist : any; //Qui salverò la traccia selezionata
+    spotifyServiceObs : Observable<Object>;
+
+
+  //Usiamo la dependency injection per farci mandare i moduli del routing e dello
+  //SpotifyService
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: SpotifyService,
+    public location : Location ) { }
+
 
   ngOnInit(): void {
+    //Ottengo l'observable che notifica le informazioni sulla route attiva
+    this.routeObs = this.route.paramMap;
+    this.routeObs.subscribe(this.getRouterParam);
+  }
+
+  //Ogni volta che viene invocata la route tracks/:id, l'observable richiama questo metodo
+  getRouterParam = (params: ParamMap) =>
+  {
+    let artistId = params.get('id'); //Ottengo l'id dai parametri
+    console.log (artistId); //Stampo su console
+    //spotifyServiceObs va dichiarato
+    this.spotifyServiceObs = this.service.getArtist(artistId) ;
+    this.spotifyServiceObs.subscribe((data)=>this.artist = data)
+  }
+
+
+back() : void
+  {
+      this.location.back();
   }
 
 }
+
