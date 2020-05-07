17d0eb8ed83bdf815de0cb3099d3c301648cd34d
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2020-05-07 15:19:45 +0000

    continuazione progetto finale

--- angular-routing/src/app/app.module.ts
+++ angular-routing/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumComponent } from './album/album.component';
@@ -8,6 +8,31 @@ import { ArtistComponent } from './artist/artist.component';
 import { TrackComponent } from './track/track.component';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
+import { InfoService }  from './info.service';
+import { UserComponent } from './user.component';
+import { LoginComponent } from './login.component';
+import { SpotifyAuthModule } from 'spotify-auth';
+import { SpotifyAuthInterceptor2 } from './spotify-auth.interceptor';
+import { Routes, RouterModule, Router } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'user',
+    pathMatch: 'full'
+  },
+  {
+    path: 'user',
+    component: UserComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  SpotifyAuthModule.authRoutes()[0]
+];
+
 
 @NgModule({
   declarations: [
@@ -22,8 +47,19 @@ import { SearchComponent } from './search/search.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    SpotifyAuthModule.forRoot(),
+    RouterModule.forRoot(routes, {
+      // useHash: true
+    }),
   ],
-  providers: [],
+providers: [
+    InfoService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SpotifyAuthInterceptor2, //Force interception.
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
